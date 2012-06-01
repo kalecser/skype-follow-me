77e12855b7f274c129d524ba01e4cc9fc48fc735
kalecser <kalecser@gmail.com>
2012-06-01 15:41:50 -0300

    increasing verbosity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,17 @@ public class Main {
 	public static void main(String[] args) throws SkypeException, InterruptedException {
 		
 		if (args.length != 1) System.out.println("Usage java -jar skype-follow-me.jar <destination>");
-		
 		final String destination = args[0];
-		
-		FollowMe subject = new FollowMe();
-		subject.redirectAllMessagesTo(destination);
+		FollowMe subject = redirectAllMessagesTo(destination);
+		System.out.println("Redirecting all Skype message to: " + destination);
 		keepRunning(subject);
 		
+	}
+
+	private static FollowMe redirectAllMessagesTo(final String destination) {
+		FollowMe subject = new FollowMe();
+		subject.redirectAllMessagesTo(destination);
+		return subject;
 	}
 
 	private static void keepRunning(FollowMe subject)
