bd9f556d4580e6eb8c0361e7d210597ce94765ff
kalecser <kalecser@gmail.com>
2012-06-03 13:24:41 -0300

    Gui progress

--- src/test/java/org/kalecser/skype/ui/Operator.java
+++ src/test/java/org/kalecser/skype/ui/Operator.java
@@ -10,28 +10,31 @@ import org.netbeans.jemmy.operators.JFrameOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
 
 class Operator{
-private JFrameOperator screen;
-JTextFieldOperator destination = new JTextFieldOperator(screen);
-JButtonOperator startRedirectButton = new JButtonOperator(screen);
-Operator(NeedsMock needs) {
-	SkypeFollowMeScreen subject = new SkypeFollowMeScreen(needs);
-	subject.show();
-	screen = new JFrameOperator(SkypeFollowMeScreen.TITLE);
-}
+	private JFrameOperator screen;
+	JTextFieldOperator destination;
+	JButtonOperator startRedirectButton;
 
-void redirectAllMessagesTo(String text) {
-	destination.typeText(text);
-	startRedirectButton.doClick();
-	waitForEventDispatch();
-}
-
-private void waitForEventDispatch() {
-	try {
-		SwingUtilities.invokeAndWait(new Runnable() {  @Override public void run() {}});
-	} catch (InterruptedException e) {
-		throw new UnhandledException(e);
-	} catch (InvocationTargetException e) {
-		throw new UnhandledException(e);
+	Operator(NeedsMock needs) {
+		SkypeFollowMeScreen subject = new SkypeFollowMeScreen(needs);
+		subject.show();
+		screen = new JFrameOperator(SkypeFollowMeScreen.TITLE);
+		destination = new JTextFieldOperator(screen);
+		startRedirectButton = new JButtonOperator(screen);
 	}
-}
+	
+	void redirectAllMessagesTo(String text) {
+		destination.typeText(text);
+		startRedirectButton.doClick();
+		waitForEventDispatch();
+	}
+	
+	private void waitForEventDispatch() {
+		try {
+			SwingUtilities.invokeAndWait(new Runnable() {  @Override public void run() {}});
+		} catch (InterruptedException e) {
+			throw new UnhandledException(e);
+		} catch (InvocationTargetException e) {
+			throw new UnhandledException(e);
+		}
+	}
 }
